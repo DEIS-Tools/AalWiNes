427b16892683f4329317e8aa40fdb89ce7c7be31
Morten Konggaard Schou <mschou15@student.aau.dk>
2020-04-06 14:05:45 +0200

    Extend make_network with coordinates

--- src/aalwines/model/Network.h
+++ src/aalwines/model/Network.h
@@ -67,6 +67,7 @@ public:
     void write_prex_routing(std::ostream& s);
 
     static Network make_network(const std::vector<std::string>& names, const std::vector<std::vector<std::string>>& links);
+    static Network make_network(const std::vector<std::pair<std::string,Coordinate>>& names, const std::vector<std::vector<std::string>>& links);
 private:
     // NO TOUCHEE AFTER INIT!
     routermap_t _mapping;
